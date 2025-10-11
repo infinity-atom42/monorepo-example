03428efbbc446760649b386c5751b0eb2e0980e7
Infinity Atom <19624654+infinity-atom42@users.noreply.github.com>
2025-10-11 12:25:53 +0300

    Remove commented `useSecureCookies` option in authentication configuration for server and web examples

--- apps/web-example/src/utils/auth.ts
+++ apps/web-example/src/utils/auth.ts
@@ -18,7 +18,6 @@ export const auth: BetterAuth = betterAuth({
 			enabled: true,
 			domain: clientEnv.NEXT_PUBLIC_BASE_URL, // your domain
 		},
-		// useSecureCookies: true, // TODO: how to make this work?
 	},
 	trustedOrigins: [clientEnv.NEXT_PUBLIC_EXAMPLE_API_URL],
 	plugins: [
